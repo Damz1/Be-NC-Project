797dc435e802ade484688e20bce0449a7875d3f8
David Maizil <davidmaizil@Davids-MBP.broadband>
2023-03-28 16:43:35 +0100

    task 5 completed

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,7 @@ const request = require("supertest");
 const db = require("../db/connection");
 const testData = require("../db/data/test-data");
 const seed = require("../db/seeds/seed");
+const { toBeSortedBy } = require("jest-sorted");
 
 afterAll(() => {
   return db.end();
@@ -93,6 +94,38 @@ describe("Get 200: /api/reviews/:review_id", () => {
       .expect(400)
       .then(({ body }) => {
         expect(body.msg).toBe("400: bad request");
+      });
+  });
+});
+
+describe("Get 200: /api/reviews", () => {
+  test("should respond with an array of objects reviews with all properties", () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then(({ body }) => {
+        const reviews = body.reviews;
+        expect(reviews).toBeInstanceOf(Array);
+        reviews.forEach((review) => {
+          expect(review).toHaveProperty("owner");
+          expect(review).toHaveProperty("title");
+          expect(review).toHaveProperty("review_id");
+          expect(review).toHaveProperty("category");
+          expect(review).toHaveProperty("review_img_url");
+          expect(review).toHaveProperty("created_at");
+          expect(review).toHaveProperty("votes");
+          expect(review).toHaveProperty("designer");
+          expect(review).toHaveProperty("comment_count");
+        });
+      });
+  });
+  test("should respond with an array of objects sorted by date in descending order", () => {
+    return request(app)
+      .get("/api/reviews")
+      .expect(200)
+      .then(({ body }) => {
+        const reviews = body.reviews;
+        expect(reviews).toBeSortedBy("created_at", { descending: true });
       });
   });
 });
--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const app = express();
 const {
   getCategories,
   getReviewById,
+  getReviews,
 } = require("./controllers/games.controllers");
 const { handle400Errors, handleOtherErrors } = require("./errorsHandler");
 
 app.get("/api/categories", getCategories);
 app.get("/api/reviews/:review_id", getReviewById);
+app.get("/api/reviews", getReviews);
 
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "404: url not found" });
