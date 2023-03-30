e8d5bd0afefde33935769069fb791ea75a9e41c8
David Maizil <davidmaizil@Davids-MBP.broadband>
2023-03-30 10:09:43 +0100

    set up production

--- listen.js
+++ listen.js
@@ -1,8 +1,9 @@
 const app = require("./app");
+const { PORT = 9090 } = process.env;
 
-app.listen(9090, (err) => {
+app.listen(PORT, (err) => {
   if (err) {
     console.log(err);
   }
-  console.log("listening on port 9090");
+  console.log(`listening on ${PORT}...`);
 });
