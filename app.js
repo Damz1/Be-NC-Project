1bc4caf89d056716b7c5875eb768c3ed62474212
David Maizil <davidmaizil@Davids-MBP.broadband>
2023-04-01 19:47:40 +0100

    refactor app.js

--- app.js
+++ app.js
@@ -17,11 +17,15 @@ app.use(express.json());
 app.get("/api", getApi);
 app.get("/api/users", getUsers);
 app.get("/api/categories", getCategories);
-app.get("/api/reviews/:review_id", getReviewById);
 app.get("/api/reviews", getReviews);
-app.get("/api/reviews/:review_id/comments", getCommentsByReviewId);
-app.post("/api/reviews/:review_id/comments", addComment);
-app.patch("/api/reviews/:review_id", updateVotes);
+
+app.route("/api/reviews/:review_id").get(getReviewById).patch(updateVotes);
+
+app
+  .route("/api/reviews/:review_id/comments")
+  .get(getCommentsByReviewId)
+  .post(addComment);
+
 app.delete("/api/comments/:comment_id", deleteComment);
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "404: url not found" });
