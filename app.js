4a6daa64f6323a46f997fc3c720535ec787ff218
David Maizil <davidmaizil@Davids-MBP.broadband>
2023-04-03 22:10:53 +0100

    refactored to add comments-router

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express();
 const routers = require("./routers/routers");
 const reviewsRouter = require("./routers/reviews-routers");
 const usersRouter = require("./routers/users-router");
+const commentsRouter = require("./routers/comments-router");
 const { handle400Errors, handleOtherErrors } = require("./errorsHandler");
 
 app.use(express.json());
 
+app.use("/api/comments", commentsRouter);
 app.use("/api/reviews", reviewsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api", routers);
--- routers/routers.js
+++ routers/routers.js
@@ -1,15 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {
-  getCategories,
-  deleteComment,
-  getApi,
-  updateCommentVote,
-} = require("../controllers/games.controllers");
+const { getCategories, getApi } = require("../controllers/games.controllers");
 
 router.get("/categories", getCategories);
-router.delete("/comments/:comment_id", deleteComment);
-router.patch("/comments/:comment_id", updateCommentVote);
+
 router.get("/", getApi);
 
 module.exports = router;
